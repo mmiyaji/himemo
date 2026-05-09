9a9efaa17b14abef431eabc4efcdd8d2e55f02d5
mmiyaji <ruhenheim1223@gmail.com>
2026-05-09 18:57:39 +0900

    Reduce note editor timestamp size

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -11601,7 +11601,8 @@ class _NoteEditorSheetState extends ConsumerState<_NoteEditorSheet> {
                         Expanded(
                           child: Text(
                             '${_createdAt.year}/${_createdAt.month.toString().padLeft(2, '0')}/${_createdAt.day.toString().padLeft(2, '0')} ${_createdAt.hour.toString().padLeft(2, '0')}:${_createdAt.minute.toString().padLeft(2, '0')}',
-                            style: Theme.of(context).textTheme.headlineSmall,
+                            style: Theme.of(context).textTheme.titleLarge
+                                ?.copyWith(fontWeight: FontWeight.w700),
                           ),
                         ),
                         Icon(
